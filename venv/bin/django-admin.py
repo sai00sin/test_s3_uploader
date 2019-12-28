91e40ef05a49733a0791283cf5c93156ac08dab6
井上幸信 <saishininoue@gmail.com>
2019-12-29 01:18:15 +0900

    Initial Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/user/Documents/working/python/django/s3uploader/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
